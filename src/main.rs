f6028fb6523561d5e74e51657f0fd8cfb2f1ee0e
Marco Antonio Santana <marco@santana.nyc>
2023-10-01 20:19:40 -0400

    testing why is it all one file?

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,6 @@ impl fmt::Display for Person {
     }
 }
 
-
 fn main() {
     let mut people: Vec<Person> = Vec::new();
     let mut p2: Vec<Person> = Vec::new();
@@ -109,4 +108,68 @@ fn main() {
             }
         }
     }
+}
+
+#[cfg(test)]
+mod swap {
+    // use crate::Person;
+    #[test]
+    fn test_swap() {
+        // swap two
+        // check that the bodies are the same, but
+        // the minds wave swapped..
+        // propper check should store the previous value of everything
+        // and insure everything changed properly after the swap.
+        assert!(true);
+    }
+}
+
+#[cfg(test)]
+mod can_swap {
+    use crate::Person;
+    // two people who have never swapped
+    #[test]
+    fn two_new_persons() {
+        let person_a = Person::new();
+        let person_b = Person::new();
+        assert!( Person::can_swap(&person_a, &person_b) );
+    }
+
+    // one person who has never swapped
+    #[test]
+    fn one_new_person() {
+        let person_a = Person::new();
+        let mut person_b = Person::new();
+        let mut person_c = Person::new();
+        Person::swap(&mut person_b, &mut person_c);
+        assert!( Person::can_swap(&person_a, &person_b) );
+    }
+
+    // two people that have swapped but not with each other
+    #[test]
+    fn two_swappable_persons() {
+        let mut person_a = Person::new();
+        let mut person_b = Person::new();
+        let mut person_c = Person::new();
+        let mut person_d = Person::new();
+        Person::swap(&mut person_b, &mut person_c);
+        Person::swap(&mut person_a, &mut person_d);
+        assert!( Person::can_swap(&person_a, &person_b) );
+    }
+
+    // two people who have swapped with each other
+    #[test]
+    fn two_unswappable_persons() {
+        let mut person_a = Person::new();
+        let mut person_b = Person::new();
+        let mut person_c = Person::new();
+        let mut person_d = Person::new();
+        // shuffle
+        Person::swap(&mut person_b, &mut person_c);
+        Person::swap(&mut person_a, &mut person_d);
+        Person::swap(&mut person_a, &mut person_b);
+        Person::swap(&mut person_b, &mut person_d);
+        assert!( !Person::can_swap(&person_a, &person_b) );
+    }
+
 }
